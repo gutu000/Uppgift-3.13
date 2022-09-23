1b0450e4747b7942e7ec7a2412476dd521459179
Pingvu <89245202+Pingvu@users.noreply.github.com>
2022-09-23 14:42:22 +0200

    Add project files.

---
+++ Uppgift 3.13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Uppgift_3._13
+{
+    class program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta");
+            }
+            else if (ålder >= 15 && ålder <= 20)
+            {
+                Console.WriteLine("Du får inte delta");
+            }
+        }
+    }
+}
